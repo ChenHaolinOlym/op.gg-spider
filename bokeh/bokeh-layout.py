4cb463c0e5f5e63340bfb378f45f074283e02e1c
ChenHaolinOlym <1024792636@qq.com>
2019-07-06 01:03:22 +0800

    initiallize bokeh imports

--- bokeh/bokeh-layout.py
+++ bokeh/bokeh-layout.py
@@ -1,5 +1,11 @@
-import bokeh
-
+from bokeh.layouts import widgetbox, layout, Row
+from bokeh.plotting import curdoc, figure
+from bokeh.models import ColumnDataSource
+from bokeh.models.widgets import Panel, Tabs, RadioButtonGroup, TextInput
+from bokeh.models.widgets import RadioGroup, Select, Paragraph
+from bokeh.models.widgets import Button, DataTable, TableColumn
+from bokeh.core.properties import value
+from functools import partial
 
 
 
