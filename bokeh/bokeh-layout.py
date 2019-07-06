c38c9c29018b887d6d63146952c2b5abe4a942bc
ChenHaolinOlym <1024792636@qq.com>
2019-07-06 18:19:30 +0800

    change the way to achieve multiline plot

--- bokeh/bokeh-layout.py
+++ bokeh/bokeh-layout.py
@@ -1,8 +1,10 @@
+# issue: initiallize a language processer
+
 from bokeh.layouts import widgetbox, layout, Row
 from bokeh.plotting import curdoc, figure
 from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Panel, Tabs, RadioButtonGroup, TextInput
-from bokeh.models.widgets import RadioGroup, Select, Paragraph
+from bokeh.models.widgets import RadioGroup, Select, Paragraph, PreText
 from bokeh.models.widgets import Button, DataTable, TableColumn
 from bokeh.core.properties import value
 from functools import partial
@@ -10,10 +12,12 @@ from functools import partial
 def ChamStatData():
     pass
 
+pre = PreText(text="Korea - Version: 9.13",width=200, height=100)
 
-plot = figure(plot_width=500, plot_height=500)
-plot.multi_line([[1, 2, 3, 4, 5, 6, 7], [5, 9, 3]],[[5, 4, 9, 2, 6, 2, 0], [7, 3, 4]],
-    line_alpha=['1.0','0.2'], line_color=['blue', 'red'], line_width=[1, 10])
+source = ColumnDataSource({'x' :[1, 2, 3, 4], 'y1':[3, 5, 2, 7], 'y2':[4, 7, 5, 9]})
+plot = figure(plot_width=1000, plot_height=500)
+plot.line('x','y1',  source=source, line_color="red")
+plot.line('x','y2',  source=source, line_color="blue")
 
 
 
@@ -21,8 +25,13 @@ plot.multi_line([[1, 2, 3, 4, 5, 6, 7], [5, 9, 3]],[[5, 4, 9, 2, 6, 2, 0], [7, 3
 # layout
 
 page = layout(
-    [widgetbox(plot, width=1000)]       
+    [widgetbox(pre, width=1000)], 
+    [widgetbox(plot, height=500, width=1000)]       
 )
 
 # display & interaction
 curdoc().add_root(page)
+
+
+
+# bokeh serve --show bokeh/bokeh-layout.py
