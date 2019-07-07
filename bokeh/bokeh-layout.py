038805a1211497edfb05e544ede75589825546ab
ChenHaolinOlym <1024792636@qq.com>
2019-07-07 15:49:17 +0800

    add tabs in the layout

--- bokeh/bokeh-layout.py
+++ bokeh/bokeh-layout.py
@@ -1,5 +1,7 @@
 # issue: initiallize a language processer
 
+
+# imports
 from bokeh.layouts import widgetbox, layout, Row
 from bokeh.plotting import curdoc, figure
 from bokeh.models import ColumnDataSource
@@ -10,6 +12,10 @@ from bokeh.core.properties import value
 from functools import partial
 import pandas as pd
 
+
+
+## function definitions
+# callbacks
 def ChamStatData():
     pass
 
@@ -20,16 +26,93 @@ def onClick(part, value):
     if part == 'Type':
         pass
 
-btnGroupPosition = RadioButtonGroup(labels=['All', 'Top', 'Jug', 'Mid', 'ADC', 'Sup'], active=0)
-btnGroupType = RadioButtonGroup(labels=['Win Rate', 'Pick Rate', 'Ban Rate'], active=0)
+def onChange(part, attr, old, new):
+    pass
+
+# widget gettings
 
 
-btnGroupPosition.on_click(partial(onClick, 'Position'))
-btnGroupType.on_click(partial(onClick, 'Type'))
 
 
-patch = Paragraph(text="Korea - Version: 9.13")
+# layouts
+patch = Paragraph(text="Korea - Version : 9.13")
 
+
+
+
+
+
+
+## Change Chart -- layout1
+# widgets
+btnGroupPosition1 = RadioButtonGroup(labels=['All', 'Top', 'Jug', 'Mid', 'ADC', 'Sup'], active=0)
+table_data = dict(
+    course_ID=[1, 2, 3, 4],
+    title=[6, 3, 8, 3],
+    department=[6, 0, 3, 2],
+    credit=[4, 8, 3, 0],
+    instructor=[5, 2, 6, 4]
+    )
+columns = [
+        TableColumn(field="course_ID", title="Course ID"),
+        TableColumn(field="title", title="Title"),
+        TableColumn(field="department", title="Department"),
+        TableColumn(field="credit", title="Credit"),
+        TableColumn(field="instructor", title="Instructor")
+    ]
+data_source = ColumnDataSource(table_data)
+table1 = DataTable(source=data_source, columns=columns, width=1000, height=1000)
+
+# callbacks
+btnGroupPosition1.on_click(partial(onClick, 'Position1'))
+
+
+
+
+
+## Change Plot -- layout2
+# widgets
+btnGroupPosition2 = RadioButtonGroup(labels=['All', 'Top', 'Jug', 'Mid', 'ADC', 'Sup'], active=0)
+btnGroupType1 = RadioButtonGroup(labels=['Win Rate', 'Pick Rate', 'Ban Rate'], active=0)
+
+# callbacks
+btnGroupPosition2.on_click(partial(onClick, 'Position2'))
+btnGroupType1.on_click(partial(onClick, 'Type1'))
+
+
+
+
+
+## Data Chart -- layout3
+# widgets
+btnGroupPosition3 = RadioButtonGroup(labels=['All', 'Top', 'Jug', 'Mid', 'ADC', 'Sup'], active=0)
+table_data = dict(
+    course_ID=[1, 2, 3, 4],
+    title=[6, 3, 8, 3],
+    department=[6, 0, 3, 2],
+    credit=[4, 8, 3, 0],
+    instructor=[5, 2, 6, 4]
+    )
+columns = [
+        TableColumn(field="course_ID", title="Course ID"),
+        TableColumn(field="title", title="Title"),
+        TableColumn(field="department", title="Department"),
+        TableColumn(field="credit", title="Credit"),
+        TableColumn(field="instructor", title="Instructor")
+    ]
+data_source = ColumnDataSource(table_data)
+table2 = DataTable(source=data_source, columns=columns, width=1000, height=1000)
+
+
+# callbacks
+btnGroupPosition3.on_click(partial(onClick, 'Position3'))
+
+
+
+## Data Plot -- layout4
+# widgets
+btnGroupPosition2 = RadioButtonGroup(labels=['All', 'Top', 'Jug', 'Mid', 'ADC', 'Sup'], active=0)
+btnGroupType4 = RadioButtonGroup(labels=['Win Rate', 'Pick Rate', 'Ban Rate'], active=0)
 source = ColumnDataSource({'x' :[1, 2, 3, 4], 'y1':[3, 5, 2, 7], 'y2':[4, 7, 5, 9]})
 plot = figure(plot_width=1000, plot_height=600,
     x_range=[0, 10], y_range=[0, 10],
@@ -38,6 +121,51 @@ plot.line('x','y1',  source=source, line_color="red")
 plot.line('x','y2',  source=source, line_color="blue")
 
 
+# callbacks
+btnGroupPosition2.on_click(partial(onClick, 'Position2'))
+btnGroupType4.on_click(partial(onClick, 'Type4'))
+
+
+
+
+
+
+
+
+
+
+# layouts
+layout1 = layout(
+    table1
+)
+
+layout2 = layout(
+    [widgetbox(btnGroupType4), widgetbox(btnGroupPosition2)],
+    [widgetbox(patch, plot, height=500, width=1000)]
+)
+
+layout3 = layout(
+    table2
+)
+
+layout4 = layout(
+    [widgetbox(btnGroupType4), widgetbox(btnGroupPosition2)],
+    [widgetbox(patch, plot, height=500, width=1000)]
+)
+
+
+
+
+
+
+
+
+
+tab1 = Panel(child=layout1, title='Change Chart')
+tab2 = Panel(child=layout2, title='Change Plot')
+tab3 = Panel(child=layout3, title='Data Chart')
+tab4 = Panel(child=layout4, title='Data Plot')
+tabs = Tabs(tabs=[tab1, tab2, tab3, tab4])
 
 
 
@@ -49,9 +177,13 @@ plot.line('x','y2',  source=source, line_color="blue")
 # layout
 
 page = layout(
-    [widgetbox(btnGroupType), widgetbox(btnGroupPosition)],
-    [widgetbox(patch, plot, height=500, width=1000)],      
+    [widgetbox(tabs)]
 )
+
+# page = layout(
+#     [widgetbox(btnGroupType), widgetbox(btnGroupPosition)],
+#     [widgetbox(patch, plot, height=500, width=1000)],      
+# )
 
 # display & interaction
 curdoc().add_root(page)
