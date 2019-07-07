75a8534e6e38d9b5b4f5648b2a5925a93a414d25
ChenHaolinOlym <1024792636@qq.com>
2019-07-07 12:49:40 +0800

    change the style of the layout

--- bokeh/bokeh-layout.py
+++ bokeh/bokeh-layout.py
@@ -5,7 +5,7 @@ from bokeh.plotting import curdoc, figure
 from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Panel, Tabs, RadioButtonGroup, TextInput
 from bokeh.models.widgets import RadioGroup, Select, Paragraph, PreText
-from bokeh.models.widgets import Button, DataTable, TableColumn
+from bokeh.models.widgets import Button, DataTable, TableColumn, Slider, RangeSlider
 from bokeh.core.properties import value
 from functools import partial
 import pandas as pd
@@ -28,10 +28,10 @@ btnGroupPosition.on_click(partial(onClick, 'Position'))
 btnGroupType.on_click(partial(onClick, 'Type'))
 
 
-pre = PreText(text="Korea - Version: 9.13",width=200, height=100)
+patch = Paragraph(text="Korea - Version: 9.13")
 
 source = ColumnDataSource({'x' :[1, 2, 3, 4], 'y1':[3, 5, 2, 7], 'y2':[4, 7, 5, 9]})
-plot = figure(plot_width=1000, plot_height=500,
+plot = figure(plot_width=1000, plot_height=600,
     x_range=[0, 10], y_range=[0, 10],
     x_axis_label='Days', x_axis_type='datetime', y_axis_label='percent of ')
 plot.line('x','y1',  source=source, line_color="red")
@@ -49,9 +49,8 @@ plot.line('x','y2',  source=source, line_color="blue")
 # layout
 
 page = layout(
-    [widgetbox(pre, width=1000)], 
-    [widgetbox(plot, height=500, width=1000)],
-    [widgetbox(btnGroupPosition, btnGroupType)]       
+    [widgetbox(btnGroupType), widgetbox(btnGroupPosition)],
+    [widgetbox(patch, plot, height=500, width=1000)],      
 )
 
 # display & interaction
